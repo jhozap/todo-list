8254df7a3808abe5b87aabdd79202202bb29964c
Jzapata <jhonny.z1022@gmail.com>
2021-11-04 19:05:04 -0500

    Container

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FormTodo from './FormTodo'
 import TaskList from './TaskList'
 
 const Container = () => {
+
+    const [list, setList] = useState([]);
+
+    const handleAddItem = addItem => {
+        setList([...list, addItem]);
+      };
+
     return (
         <div>
-            <FormTodo />
-            <TaskList />
+            <FormTodo handleAddItem={handleAddItem} />
+            <TaskList list={list} setList={setList} />
         </div>
     )
 }
