4e01644fcab1c51acf3ae2e22bfd862321383169
Jzapata <jhonny.z1022@gmail.com>
2021-11-04 19:16:02 -0500

    checkbox

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Fragment } from 'react';
 
-const Checkbox = () => {
+const Checkbox = (props) => {
+
+    const {
+        onChange,
+        data: { id, description, done }
+    } = props;
+
+
     return (
-        <div>
-            <h1>Checkbox</h1>
-        </div>
+        <Fragment>
+            <label className="todo new-item">
+                <input
+                    className="todo__state"
+                    name={id}
+                    type="checkbox"
+                    defaultChecked={done}
+                    onChange={onChange}
+                />
+                <div className="todo__text">{description}</div>
+            </label>
+        </Fragment>
     )
 }
 
